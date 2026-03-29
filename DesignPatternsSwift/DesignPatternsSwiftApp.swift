d05508a1ae40e8e35fdea996f2fb651d5288dd77
Ivan Pereira <ivanexdev@gmail.com>
2026-03-29 13:01:50 -0300

    Initial Commit

---
+++ DesignPatternsSwift/DesignPatternsSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignPatternsSwiftApp.swift
+//  DesignPatternsSwift
+//
+//  Created by Ivan Pereira on 29-03-26.
+//
+
+import SwiftUI
+
+@main
+struct DesignPatternsSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
